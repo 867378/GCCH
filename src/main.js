55467d5f6d9634b75db8de3c0092bea7afb99bd7
Kyu020 <aaronjanestacio20@gmail.com>
2025-05-21 11:40:37 +0800

    try mo nga toh

--- src/main.js
+++ src/main.js
@@ -4,26 +4,29 @@ import { initializeCsrfCookie } from './plugins/sanctum';
 
 import App from "./App.vue";
 import router from "./router";
-import axios from "axios";
+// import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
+// axios.defaults.baseURL = "http://localhost:8000/api";
+// axios.defaults.withCredentials = true;
+// axios.defaults.withXSRFToken = true;
 
 const app = createApp(App);
 
-async function bootstrap(){
-    try {
-        await initializeCsrfCookie();
-        app.use(createPinia());
-        app.use(router);
-        app.mount("#app");
-    } catch (error) {
-        console.error("Failed to initialize CSRF cookie", error);
-    }
-}
+// async function bootstrap(){
+//     try {
+//         await initializeCsrfCookie();
+//         app.use(createPinia());
+//         app.use(router);
+//         app.mount("#app");
+//     } catch (error) {
+//         console.error("Failed to initialize CSRF cookie", error);
+//     }
+// }
 
-bootstrap();
+// bootstrap();
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
 
 // Disable devtools
 app.config.devtools = false;
--- src/plugins/sanctum.js
+++ src/plugins/sanctum.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export async function initializeCsrfCookie() {
-  try {
-    await axios.get('http://localhost:8000/sanctum/csrf-cookie');
-    console.log('CSRF cookie initialized');
-  } catch (error) {
-    console.error('Failed to initialize CSRF cookie', error);
-    throw error;
-  }
-}
+// export async function initializeCsrfCookie() {
+//   try {
+//     await axios.get('http://localhost:8000/sanctum/csrf-cookie');
+//     console.log('CSRF cookie initialized');
+//   } catch (error) {
+//     console.error('Failed to initialize CSRF cookie', error);
+//     throw error;
+//   }
+// }
